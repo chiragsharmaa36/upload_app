3e8975792611b2de0784b0de3de09504e23b71f9
Chirag <chirag.sharmaa36@gmail.com>
2023-05-04 18:57:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react"
+import useDrivePicker from "react-google-drive-picker"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [openPicker, data, authResponse] = useDrivePicker();
+
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId:"1049157451039-mlumkpl8ab4qug5qfvq26iulvgblmkcg.apps.googleusercontent.com",
+      developerKey: "AIzaSyCbxS_HBadMjJa_ktC4Ax8Q59bmkKL8Aqc",
+      viewId: "DOCS",
+      token:"ya29.a0AWY7Ckn7j-1bLIVRSvFL36AOFqOr2Kom2gS76loDGZbBY6JYwOBZCzMFIkkEA9FeZsQBVX2PiR6Q9pd6tDkp2JI2BmWGKAl-5n6_kBe25rif4kttBdYiEEglcQzQjTmhccN0tJonGoQVRd4WAazGhJcSmdHYaCgYKAYkSARMSFQG1tDrp3x40oQjioMax1KRcig2AGw0163",
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      multiselect: true
+    })
+  }
+
+  useEffect(() =>{
+    if(data)
+    {
+      data.docs.map((i) => console.log(i))
+    }
+  }, [data])
+
+return (
+    <div>
+        <button onClick={() => handleOpenPicker()}>Open Picker</button>
     </div>
   );
+
 }
 
 export default App;
